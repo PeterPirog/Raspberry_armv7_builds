5faf45170c2855e1c9445cbd5504c4eceb48f7da
PeterPirog <peterpirogtf@gmail.com>
2021-02-07 17:38:50 +0100

    added Pillow libraries script

---
+++ Pillow/install_libraries.sh
@@ -0,0 +1,9 @@
+sudo apt-get install -y libjpeg-dev zlib1g-dev libtiff-dev libfreetype6
+sudo apt-get install -y libwebp-dev
+sudo apt-get install -y tcl
+sudo apt-get install -y libimagequant-dev
+sudo apt-get install -y  libraqm-dev
+sudo apt-get install -y libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev
+sudo apt install -y libopenjp2-7-dev
+sudo apt-get install -y liblcms2-utils
+sudo apt-get install -y liblcms2-dev
